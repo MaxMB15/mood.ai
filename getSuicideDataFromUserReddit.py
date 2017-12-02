c5fbd56c54bf59269ffeeb9faad5a57cd48f3d5d
Max <MaxMB15@gmail.com>
2017-12-02 07:04:05 -0500

    script now complete with an example training set

--- getSuicideDataFromUserReddit.py
+++ getSuicideDataFromUserReddit.py
@@ -2,29 +2,24 @@ import sys
 
 import praw
 
-#python subredditTitle pathOfCSV
-if (len(sys.argv) != 3):
-	raise "invalid number of arguments. There must be 2, there is %d" %(len(sys.argv)-1)
+#python subredditTitle pathOfCSV riskOfSuisideNumber
+if (len(sys.argv) != 4):
+	raise "invalid number of arguments. There must be 3, there is %d" %(len(sys.argv)-1)
 print sys.argv[1]
 
 reddit = praw.Reddit(user_agent='Comment Extraction (by /u/MaxMB15)',
                      client_id='81NfLOyVODsR7g', client_secret="260X4gdtAeFhtHZZEKVH_r8lKlA",
                      username='MaxMB15', password='tempPass')
 
-subreddit = reddit.subreddit('depression_help')
+#append to the CSV file
+import csv
 
-for submission in reddit.subreddit('depression_help').hot(limit=200):
-    print(submission.title)
+for submission in reddit.subreddit(sys.argv[1]).hot(limit=500):
+    print (submission.title)
 
-
-
-
-
-
-
-
-
-
-
-
-
+csvfile = open(sys.argv[2], 'ab')
+filewriter = csv.writer(csvfile, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
+for submission in reddit.subreddit(sys.argv[1]).hot(limit=500):
+	if submission.title.encode('ascii', 'ignore') != "":
+    		filewriter.writerow([submission.title.encode('ascii', 'ignore'), sys.argv[3]])
+csvfile.close()
